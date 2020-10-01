fea14347fc2c7b4d4d5f4fee21c46ee921ae96f7
Abdalaziz Mohamed <a.mohamed@CAdmins-MacBook-Pro.local>
2020-10-01 17:41:57 +0200

    chore: Add comment to the code bankocr.py

--- bankocr.py
+++ bankocr.py
@@ -1,6 +1,7 @@
 #!/bin/python
 import sys
 
+# Map the value of each digit in three-line as one string to its number 
 DIGIT_VALUES = {
     ' _ | ||_|': 0,
     '     |  |': 1,
@@ -14,6 +15,7 @@ DIGIT_VALUES = {
     ' _ |_| _|': 9
 }
 
+# Open a file provided or ask for the file
 try:
     path = sys.argv[1]
 except IndexError as e:
@@ -27,7 +29,8 @@ else:
     with open(path, 'r+') as f:
         lines = f.read().splitlines()
 
-
+# start read every 4 lines, three-line which contain the digit number
+# on each line combines three char which consist of the digit number to one string
 for line_number, line in enumerate(lines):
     if line_number % 4 == 0:
         completeNumber = ''
@@ -38,6 +41,7 @@ for line_number, line in enumerate(lines):
                 lines[line_number + 2][number_start:number_end]
             completeNumber += str(DIGIT_VALUES.get(number))
         try:
+            # Print the number if it is valid
             print(int(completeNumber))
         except :
             print("Error in data")
